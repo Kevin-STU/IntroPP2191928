17cb99c76cbb90ea73e3fc573224c943d0c01e78
Kevin Prada <82003549+Kevin-STU@users.noreply.github.com>
2023-09-22 23:02:22 -0500

    Se actualiza el código secuencial

--- OpenMP/sumfibonacci.c
+++ OpenMP/sumfibonacci.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include "omp.h"
 
+/* Se utiliza unsigned long long para el 
+tipo de datos debido a la necesidad de manejar 
+números grandes sin signo en este código. */
 unsigned long long calculateEvenSum(int n)
 {
-    unsigned long long sum=0, fibo[2*n + 1];
+    // Se utiliza 2n+1 en el tamaño del arreglo fibo[] porque necesitamos 
+    // almacenar los números de Fibonacci desde 0 hasta 2n, lo que suma un total de 2n+1 números.
+    unsigned long long sum=0, fibo[2*n + 1]; 
     fibo[0]=0, fibo[1]=1;
 
+    // Se calculan los números de Fibonacci hasta 2*n
     for(int i=2; i <= 2*n; i++)
     {
         fibo[i] = fibo[i-1] + fibo[i-2];
     }
-
+    
+    // Se calcula la suma de los números de Fibonacci en índices pares
     for(int i=0; i <= 2*n; i+=2)
     {
         sum+=fibo[i];
